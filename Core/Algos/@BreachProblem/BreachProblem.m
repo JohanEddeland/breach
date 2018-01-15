84e5f7e887bda94285a0a8c936c52baf05e8ca30
Cluster 4374 <johan.eddeland@volvocars.com>
2018-01-15 10:46:10 +0100

    Changed number of initial samples used for QuasiRandomSample (used by solve_global_nelder_mead.m)

--- Core/Algos/@BreachProblem/BreachProblem.m
+++ Core/Algos/@BreachProblem/BreachProblem.m
@@ -405,7 +405,7 @@ classdef BreachProblem < BreachStatus
                 % Too many parameter combinations to enumerate
                 % Use QuasiRandomSample instead
                 if ~exist('n_samples', 'var')
-                    n_samples = floor(2*this.max_time);
+                    n_samples = floor(0.1*this.max_time);
                 end
                 fprintf('%d varying parameters, using QuasiRandomSample(%d) (TestronRefine) to not run out of memory\n',numel(this.params),n_samples);
                 BrC.QuasiRandomSample(n_samples);
