c9e41f8c8448e2c904bfff3ff1fca3d764db2d47
Cluster 4374 <johan.eddeland@volvocars.com>
2018-02-13 14:29:39 +0100

    Now only gives a warning (and displays a message) when Simulink returns an error

--- Core/BreachSimulinkSystem.m
+++ Core/BreachSimulinkSystem.m
@@ -843,7 +843,8 @@ classdef BreachSimulinkSystem < BreachOpenSystem
                 else
                     tout = [0 tspan];
                 end
-                error(['An error was returned from Simulink:' s.message '\n Returning a null trajectory']);
+                warning(['An error was returned from Simulink:' s.message '\n Returning a null trajectory']);
+                disp(['WARNING: An error was returned from Simulink: ' s.message]);
                 X = zeros(Sys.DimX, numel(tout));
                 status =-1;
                 this.addStatus(-1, MException.identifier, MException.message);
