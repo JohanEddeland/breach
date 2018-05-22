0c270fe39baa34fe6651bf6d501cc3b7e3189091
Johan Eddeland <johan.eddeland@volvocars.com>
2018-05-22 11:55:51 +0200

    Additive robustness now also checks final time sample

--- @STL_Formula/private/RobustAlways.m
+++ @STL_Formula/private/RobustAlways.m
@@ -94,6 +94,10 @@ else
         for k = 1:numel(valarray)-1
             partialRob = partialRob + (1/valarray(k))*(time_values(k+1) - time_values(k));
         end
+        % Add the last time point as well
+        partialRob = partialRob + (1/valarray(end))*(time_values(end) - time_values(end-1));
+        
+        % Calculate the reciprocal
         partialRob = 1/partialRob;
     end
 end
