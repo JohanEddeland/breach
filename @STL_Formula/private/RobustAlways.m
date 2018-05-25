aa5469128a9d28204514da2d2643e3de3006a84e
Cluster 4374 <johan.eddeland@volvocars.com>
2018-05-25 10:38:43 +0200

    Fixed partialRobustAlways to include final time point as well

--- @STL_Formula/private/RobustAlways.m
+++ @STL_Formula/private/RobustAlways.m
@@ -1,6 +1,8 @@
 function [time_values, valarray] = RobustAlways(time_values, valarray, I___)
 
 indicesToRemove = [];
+time_values = time_values - I___(1);
+I___ = I___ - I___(1);
 
 for valIndex = 1:length(valarray)
     thisTime = time_values(valIndex);
@@ -18,25 +20,27 @@ for valIndex = 1:length(valarray)
     
     if isempty(partialTime)
         % There is no signal in the time we are looking at
-        % Just remove this point from valarray and time_values
-        time_values(valIndex) = [];
-        valarray(valIndex) = [];
+        % We have to find the signal value from the point defined before
+        % this one
+        timeIntervalIndex = find(time_values <= endTime, 1, 'last');
+        partialTime = time_values(timeIntervalIndex);
+        partialValarray = valarray(timeIntervalIndex);
+    end
+    
+    partialRob = PartialRobustAlways(partialTime, partialValarray);
+    
+    % Check if we can reduce the size of time_values and valarray by
+    % removing this element (if it is the same as the element before
+    if valIndex == 1 || valIndex == length(valarray)
+        % We cannot remove the first or the last element
+        valarray(valIndex) = partialRob;
+    elseif valarray(valIndex - 1) == partialRob
+        % The value is the same as the previous one
+        % We don't need to add a new value - instead, we can remove this
+        % time step from the time_values and valarray vectors later on
+        indicesToRemove(end+1) = valIndex; %#ok<*AGROW>
     else
-        partialRob = PartialRobustAlways(partialTime, partialValarray);
-        
-        % Check if we can reduce the size of time_values and valarray by
-        % removing this element (if it is the same as the element before
-        if valIndex == 1 || valIndex == length(valarray)
-            % We cannot remove the first or the last element
-            valarray(valIndex) = partialRob;
-        elseif valarray(valIndex - 1) == partialRob
-            % The value is the same as the previous one
-            % We don't need to add a new value - instead, we can remove this
-            % time step from the time_values and valarray vectors later on
-            indicesToRemove(end+1) = valIndex; %#ok<*AGROW>
-        else
-            valarray(valIndex) = partialRob;
-        end
+        valarray(valIndex) = partialRob;
     end
     
 end
@@ -58,7 +62,7 @@ if numel(valarray) == 1
 end
 
 
-rho = min(valarray(1:end-1));
+rho = min(valarray(1:end));
 
 if rho < 0
     % The spec fails - we will integrate over the faulty intervals
@@ -70,12 +74,15 @@ if rho < 0
     %         partialRob = partialRob + valLessThanZero(k)*(timesLessThanZero(k+1) - timesLessThanZero(k));
     %     end
     partialRob = 0;
-    totalTimeIntegrated = 0;
     for k = 1:numel(valarray)-1
         if valarray(k) < 0
             partialRob = partialRob + valarray(k)*(time_values(k+1) - time_values(k));
-            totalTimeIntegrated = totalTimeIntegrated + (time_values(k+1) - time_values(k));
         end
+    end
+    
+    % Add the last time point as well (if it fails)
+    if valarray(end) < 0
+        partialRob = partialRob + valarray(end)*(time_values(end) - time_values(end-1));
     end
     
     % Assert that the partialRob is negative - otherwise our additive
