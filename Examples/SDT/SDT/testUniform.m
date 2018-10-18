43b2aaa0a8be7d4b8139a6c193deea5d20e547f9
Thao Dang <thao.dang@univ-grenoble-alpes.fr>
2018-10-19 01:24:48 +0200

    changed sdt

--- Examples/SDT/SDT/testUniform.m
+++ Examples/SDT/SDT/testUniform.m
@@ -7,6 +7,7 @@ addpath('./Functions')
 addpath('./ExampleBand-Pass')
 addpath('./ExampleLow-Pass')
 addpath('./periodic.signals')
+addpath('/Users/thaodang/breach-dev')
 
 
 
@@ -20,7 +21,7 @@ BrSD = BreachSimulinkSystem(model_name);
 %, 'all', [], {}, [], 'Verbose',0,'SimInModelsDataFolder', true);
 
 
-for sigId = 10:10
+for sigId = 1:51
     if (sigId < 10)
         sigfilename = strcat('s_0', num2str(sigId));
     else
@@ -28,7 +29,8 @@ for sigId = 10:10
     end
     
     In1 = load(sigfilename, '-ascii');
-    In1(:,1) = 1e-7*In1(:,1); 
+    scaling=1e-8;
+    In1(:,1) = scaling*In1(:,1); 
     time = In1(:,1);
     time= time-time(1);
     sg_in = from_workspace_signal_gen({'In1'});
@@ -39,22 +41,25 @@ for sigId = 10:10
     
     
     %BrSD.SetTime([0 sim_time]);
-    %phi = STL_Formula('saturation', 'alw_[0, sim_time] (OutSat[t] < 0.1)');
-    %phi = set_params(phi, {'sim_time'}, time(end));
+    phi = STL_Formula('notsaturation', 'alw_[0, sim_time] (Out1[t] < 2)');
+    phi = set_params(phi, {'sim_time'}, time(end));
     
     BrSD_temp.Sim(time);
     %input('Press ENTER to continue');
-    BrSD_temp.PlotSignals({'In1', 'OutSat'});
+    %new_fig = gcf;
+    BrSD_temp.PlotSignals({'In1', 'Out1'});
     
     
-    %rob = BrSD_temp.CheckSpec(phi);
-%     if rob<0
-%         disp('Falsified in StatFalsify');
-%         new_fig = gcf;
-%         BrSD_temp.PlotSignals({'In1', 'OutSat'});
-%         falsified = true;
-%         return;
-%     end
+    rob = BrSD_temp.CheckSpec(phi)
+    sigId
+    if rob<0
+        disp('Falsified in StatFalsify');
+        
+        %BrSD_temp.PlotSignals({'In1', 'OutSat'});
+        falsified = true;
+        sigId
+        break;
+    end
     
 end
 
