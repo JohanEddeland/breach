0214b534568f08eaebf23992e53d2de9e2be0bdf
Alexandre Donze <alex@decyphir.com>
2018-11-19 17:59:55 +0100

    BreachProblem R,R0, etc

--- Core/Algos/@BreachProblem/BreachProblem.m
+++ Core/Algos/@BreachProblem/BreachProblem.m
@@ -73,6 +73,8 @@ classdef BreachProblem < BreachStatus
         BrSys
         BrSet_Best
         BrSet_Logged
+        R0
+        R_log
         params
         domains
         lb
@@ -156,6 +158,7 @@ classdef BreachProblem < BreachStatus
                 phi = BreachRequirement(phi);
             end
             
+            this.R0 = phi.copy(); 
             this.Spec = phi;
             this.BrSet = BrSet.copy();
             this.BrSet.Sys.Verbose=0;
@@ -191,7 +194,7 @@ classdef BreachProblem < BreachStatus
             
             % robustness
             this.BrSys = this.BrSet.copy(); 
-            this.robust_fn = @(x) (phi.Eval(this.BrSys, this.params, x));
+            this.robust_fn = @(x) (this.Spec.Eval(this.BrSys, this.params, x));
             
             this.BrSys.Sys.Verbose=0;
              
@@ -594,7 +597,9 @@ classdef BreachProblem < BreachStatus
                     for iter = 1:nb_iter
 
                         % checks whether x has already been computed or not
-                        % can do better, but will do for now
+                        % skips logging if already computed 
+                        % might cause inconsistencies down the road...
+                        
                         if ~isempty(this.X_log)
                             xi = x(:, iter);
                             idx = find(sum(abs(this.X_log-repmat(xi, 1, size(this.X_log, 2)))) == 0,1);
@@ -602,23 +607,24 @@ classdef BreachProblem < BreachStatus
                             idx=[];
                         end
                         if ~isempty(idx)
-                            if  ~isempty(this.BrSet_Logged)
-                                this.BrSys.P = Sselect(this.BrSet_Logged.P,idx);
-                            end
+%                            if  ~isempty(this.BrSet_Logged)
+%                                this.BrSys.P =
+%                                Sselect(this.BrSet_Logged.P,idx);  
+%                                this.Spec.P = Sselect(this.R_log.P,idx); 
+%                            end
                             fval(:,iter) = this.obj_log(:,idx);
                         else
                             % calling actual objective function
                             fval(:,iter) = fun(iter);
-                        end
-
-                        % logging and updating best
-                        this.LogX(x(:, iter), fval(:,iter));
                         
-                        % update status
-                        if rem(this.nb_obj_eval,this.freq_update)==0
-                            this.display_status();
+                            % logging and updating best
+                            this.LogX(x(:, iter), fval(:,iter));
+                            
+                            % update status
+                            if rem(this.nb_obj_eval,this.freq_update)==0
+                                this.display_status();
+                            end
                         end
-                        
                         % stops if falsified or other
                         if this.stopping()
                             break
@@ -675,9 +681,12 @@ classdef BreachProblem < BreachStatus
             if (this.log_traces)&&~(this.use_parallel)&&~(this.BrSet.UseDiskCaching) % FIXME - logging flags and methods need be revised
                 if isempty(this.BrSet_Logged)
                     this.BrSet_Logged = this.BrSys.copy();
+                    this.R_log = this.Spec.copy();
                 else
                     this.BrSet_Logged.Concat(this.BrSys);
+                    this.R_log.Concat(this.Spec);
                 end
+                this.Spec = this.R0.copy();
             end
             
             [fmin , imin] = min(min(fval));
